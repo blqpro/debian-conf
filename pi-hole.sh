50e5c54ce8d37624e1e367854f8e0a32c22da619
zexeos <zexeos.github@protonmail.com>
2021-10-10 21:43:59 +0200

    Pi-hole installer BASH
    BASH script to install pi-hole on a debian based machine

---
+++ pi-hole.sh
@@ -0,0 +1,23 @@
+#!/bin/bash$
+#Install pihole 
+git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
+cd "Pi-hole/automated install/"
+sudo bash basic-install.sh
+
+read -r -p "Are you sure you want to install pi-hole on this machine  ?? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+       
+       git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
+       
+       cd "Pi-hole/automated install/"
+      
+       sudo bash basic-install.sh
+
+
+
+        ;;
+    *)
+        echo If Pi-Hole has not been installed correctly than press. Y / Yes
+        ;;
+esac
