4f216bcde81bd7ba5dd4fdd0349d33b43e237789
zexeos <zexeos.github@protonmail.com>
2021-11-05 17:57:19 +0100

    Update pi-hole.sh

--- auto-conf-script/pi-hole.sh
+++ auto-conf-script/pi-hole.sh
@@ -2,10 +2,6 @@
 
 #This BASH script will install Pi-Hole and start the auto-install script.
 
-git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
-cd "Pi-hole/automated install/"
-sudo bash basic-install.sh
-
 read -r -p "Are you sure you want to install pi-hole on this machine  ?? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
